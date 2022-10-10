67cc0b10bd8a9d635f359752171e61c1d2e8a854
harsh <harshitpandeyfeelings@gmail.com>
2022-10-10 11:45:10 +0530

    Books Class Added

--- books/Books.java
+++ books/Books.java
@@ -5,7 +5,7 @@ class Books
     String book_name[];
     Scanner sc=new Scanner(System.in);
     int total_books;
-    int id[],no_of_books[];
+    int book_id[],no_of_books[];
     Books()
     {
         System.out.println("Welcome to Library...");
@@ -20,7 +20,26 @@ class Books
         {
             System.out.println();
             System.out.println("Enter details of "+(i+1)+" book -:");
-
+            System.out.println("Enter Book ID -:");
+            book_id[i]=sc.nextInt();
+            System.out.println("Enter Book Name -:");
+            book_name[i]=sc.nextLine();
+            System.out.println("Enter total no of books -:");
+            no_of_books=sc.nextInt();
+        }
+        System.out.println();
+        System.out.println("**************");
+        System.out.println();
+        System.out.println("Value Inputted...");
+    }
+    void display()
+    {
+        for(int i=0;i<total_books;i++)
+        {
+            System.out.println();
+            System.out.println("Book Id -> "+ book_id[i]);
+            System.out.println("Book Name -> "+ book_name[i]);
+            System.out.println("Books Available -> "+no_of_books[i]);
         }
     }
 }
